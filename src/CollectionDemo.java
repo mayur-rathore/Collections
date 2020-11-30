6faaebe8d6d52ad5386b2bec96ca566568eb9542
Mayur <mayurrathore.work@gmail.com>
2020-11-30 18:05:55 +0530

    Initial commit. Collection demo class added without collection framework.

---
+++ src/CollectionDemo.java
@@ -0,0 +1,33 @@
+// Java program to demonstrate why collection framework was needed
+import java.util.Hashtable;
+import java.util.Vector;
+
+public class CollectionDemo {
+
+    public static void main(String args[]){
+
+        // Creating instances of Array
+        // Array instance creation requires []
+        int arr[] = new int[] {1, 2, 3, 4};
+
+        // Creating instance of Vector
+        // While Vector and hashtable require ()
+        Vector<Integer> vector = new Vector<Integer>();
+
+        // Creating instance of Vector
+        Hashtable<Integer, String> hashTable = new Hashtable<Integer, String>();
+
+        // Adding elements into Vector, Vector element insertion requires addElement()
+        vector.add(1);
+        vector.add(2);
+
+        // Adding elements into hashtable, hashtable element insertion requires put()
+        hashTable.put(1, "Mayur");
+        hashTable.put(2, "Mochi");
+
+        // Accessing the first element of the array, vector and hashtable
+        System.out.println("Arrays first element : " +arr[0]);
+        System.out.println("Vectors first element : " +vector.elementAt(0));
+        System.out.println("HashTable first element is : " +hashTable.get(1));
+    }
+}
