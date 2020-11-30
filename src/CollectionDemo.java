fdf3e43577a285e67ef6ad7b6cd5e5da8ebc7088
Mayur <mayurrathore.work@gmail.com>
2020-11-30 19:13:04 +0530

    Amended with Intellij Suggestions

--- src/CollectionDemo.java
+++ src/CollectionDemo.java
@@ -1,21 +1,21 @@
-// Java program to demonstrate why collection framework was needed, first file
+// Java program to demonstrate why collection framework was needed
 import java.util.Hashtable;
 import java.util.Vector;
 
 public class CollectionDemo {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         // Creating instances of Array
         // Array instance creation requires []
-        int arr[] = new int[] {1, 2, 3, 4};
+        int[] arr = new int[] {1, 2, 3, 4};
 
         // Creating instance of Vector
         // While Vector and hashtable require ()
-        Vector<Integer> vector = new Vector<Integer>();
+        Vector<Integer> vector = new Vector<>();
 
         // Creating instance of Vector
-        Hashtable<Integer, String> hashTable = new Hashtable<Integer, String>();
+        Hashtable<Integer, String> hashTable = new Hashtable<>();
 
         // Adding elements into Vector, Vector element insertion requires addElement()
         vector.add(1);
